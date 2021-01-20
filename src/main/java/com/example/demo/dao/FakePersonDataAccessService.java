e821a8fdddd18a9dba308587001ed9de846c8335
Maciej Fraszczak <maciek.fraszczak@wp.pl>
2021-01-20 02:34:19 +0100

    Mapped get function in order to be able to retrieve entries stored in database via server

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -13,8 +13,14 @@ public class FakePersonDataAccessService implements PersonDao {
 	
 	private static List<Person> DB = new ArrayList<>();
 	
-	@Override public int insertPerson(UUID id, Person person) {
+	@Override
+	public int insertPerson(UUID id, Person person) {
 		DB.add(new Person(id, person.getName()));
 		return 1;
+	}
+	
+	@Override
+	public List<Person> selectAllPeople() {
+		return DB;
 	}
 }
--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -1,5 +1,6 @@
 package com.example.demo.dao;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.example.demo.model.Person;
@@ -13,5 +14,5 @@ public interface PersonDao {
 		return insertPerson(id, person);
 	}
 	
-	
+	List<Person> selectAllPeople();
 }
